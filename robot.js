2b2f64aa1d894c7f4e314f14ae453299d090e229
sbandi5 <bandisaimani@gmail.com>
2024-10-24 22:24:22 -0400

    added methods like updating the tables in the databse

--- robot.js
+++ robot.js
@@ -1,31 +1,37 @@
-// robot.js
+
 const axios = require('axios');
 const html_parser = require('node-html-parser');
-const Tokenizer = require('./tokenizer'); // Import the Tokenizer class
-
+const Tokenizer = require('./tokenizer'); 
+const Database = require('./Database');
 class Robot {
     constructor() {
         this.isBusy = false;
         this.t = new Tokenizer(); // Instantiate Tokenizer
+        this.databaseconnection = Database.getInstance();
+        this.databaseconnection.connect();
+        this.url = [];
+        this.keywordInUrl = [];
     }
 
     async parseWebsite(Weburl,keywords) {
         this.isBusy = true;
+        let keywordCount = 0;
         try {
             let response = await axios.get(Weburl);
             let root = html_parser.parse(response.data);
-//          this.t.processContent(root.toString(),keywords,10); // Process the content using Tokenizer
-//          this.t.printExtractedData();
             let {tokens , keywordCount} = this.t.processInput(root.toString(), keywords);
             console.log(tokens + '\n');
-            console.log('The no of times '+ keywords+ ' occured in the '+Weburl+' is '+ keywordCount)
+            console.log('The no of times '+ keywords+ ' occured in the '+Weburl+' is '+ keywordCount);
+            
 
         } catch (error) {
             console.error("Error fetching the website:", error);
         } finally {
             this.isBusy = false;
+            return keywordCount;
         }
     }
+
 }
 
 module.exports = Robot;
--- tokenizer.js
+++ tokenizer.js
@@ -1,77 +1,160 @@
-class Tokenizer {
-    constructor() {
-      this.cBlank = ' ';
-      this.cCR = '\r';
-      this.cLF = '\n';
-      this.cTab = '\t';
-      this.maxLen = 1000;
-      this.token = '';
-      this.ch = this.cBlank;
-      this.openAngle = 0;
+// class Tokenizer {
+//     constructor() {
+//       this.cBlank = ' ';
+//       this.cCR = '\r';
+//       this.cLF = '\n';
+//       this.cTab = '\t';
+//       this.maxLen = 1000;
+//       this.token = '';
+//       this.ch = this.cBlank;
+//       this.openAngle = 0;
   
-      // Define HTML-like delimiters and tags
-      this.endHTMLComment = "-->";
-      this.endScript = "SCRIPT>";
-      this.startH1 = "<H1";
-      this.endH1 = "</H1>";
-      this.startMETA = "<META";
-      this.endAngleBracket = ">";
-      this.startAnchor = "<A";
-      this.endAnchor = "</A>";
-    }
+//       // Define HTML-like delimiters and tags
+//       this.endHTMLComment = "-->";
+//       this.endScript = "SCRIPT>";
+//       this.startH1 = "<H1";
+//       this.endH1 = "</H1>";
+//       this.startMETA = "<META";
+//       this.endAngleBracket = ">";
+//       this.startAnchor = "<A";
+//       this.endAnchor = "</A>";
+//     }
   
-    isWhiteSpace(ch) {
-      return (ch === this.cBlank || ch === this.cCR || ch === this.cLF || ch === this.cTab);
-    }
+//     isWhiteSpace(ch) {
+//       return (ch === this.cBlank || ch === this.cCR || ch === this.cLF || ch === this.cTab);
+//     }
   
-    delimiter(ch) {
-      return (this.isWhiteSpace(ch) || ch === this.endAngleBracket || ch === null);
-    }
+//     delimiter(ch) {
+//       return (this.isWhiteSpace(ch) || ch === this.endAngleBracket || ch === null);
+//     }
   
-    appendCharToString(ch) {
-      if (this.token.length === this.maxLen - 1) {
-        this.maxLen *= 2;
-      }
-      this.token += ch;
-    }
+//     appendCharToString(ch) {
+//       if (this.token.length === this.maxLen - 1) {
+//         this.maxLen *= 2;
+//       }
+//       this.token += ch;
+//     }
   
-    // Main function to read and process the input while counting occurrences of a keyword
-    processInput(input, keyword) {
-      let tokens = [];
-      let i = 0;
-      let keywordCount = 0;
+//     // Main function to read and process the input while counting occurrences of a keyword
+//     processInput(input, keyword) {
+//       let tokens = [];
+//       let i = 0;
+//       let keywordCount = 0;
       
-      while (i < input.length) {
-        this.ch = input[i];
+//       while (i < input.length) {
+//         this.ch = input[i];
   
+//         if (this.delimiter(this.ch)) {
+//           if (this.token.length > 0) {
+//             tokens.push(this.token);
+            
+//             // Count keyword occurrences (case-insensitive)
+//             if (this.token.toLowerCase() === keyword.toLowerCase()) {
+//               keywordCount++;
+//             }
+  
+//             this.token = ''; // Reset token
+//           }
+//         } else {
+//           this.appendCharToString(this.ch);
+//         }
+//         i++;
+//       }
+  
+//       // Push any last remaining token
+//       if (this.token.length > 0) {
+//         tokens.push(this.token);
+        
+//         // Count keyword occurrences for the last token
+//         if (this.token.toLowerCase() === keyword.toLowerCase()) {
+//           keywordCount++;
+//         }
+//       }
+  
+//       return { tokens, keywordCount };
+//     }
+//   }
+
+
+class Tokenizer {
+  constructor() {
+    this.cBlank = ' ';
+    this.cCR = '\r';
+    this.cLF = '\n';
+    this.cTab = '\t';
+    this.maxLen = 1000;
+    this.token = '';
+    this.ch = this.cBlank;
+    this.inTag = false; // Track if currently inside an HTML tag
+  }
+
+  isWhiteSpace(ch) {
+    return (ch === this.cBlank || ch === this.cCR || ch === this.cLF || ch === this.cTab);
+  }
+
+  delimiter(ch) {
+    return (this.isWhiteSpace(ch) || ch === null);
+  }
+
+  appendCharToString(ch) {
+    if (this.token.length === this.maxLen - 1) {
+      this.maxLen *= 2;
+    }
+    this.token += ch;
+  }
+
+  // Main function to read and process the input while counting occurrences of a keyword
+  processInput(input, keyword) {
+    let tokens = [];
+    let i = 0;
+    let keywordCount = 0;
+    this.token = ''; // Reset the token
+
+    while (i < input.length) {
+      this.ch = input[i];
+
+      // Check if we're entering or exiting an HTML tag
+      if (this.ch === '<') {
+        this.inTag = true;
+      } else if (this.ch === '>') {
+        this.inTag = false;
+        this.token = ''; // Reset token after finishing a tag
+        i++;
+        continue;
+      }
+
+      // Skip processing characters inside HTML tags
+      if (!this.inTag) {
         if (this.delimiter(this.ch)) {
           if (this.token.length > 0) {
             tokens.push(this.token);
-            
+
             // Count keyword occurrences (case-insensitive)
             if (this.token.toLowerCase() === keyword.toLowerCase()) {
               keywordCount++;
             }
-  
+
             this.token = ''; // Reset token
           }
         } else {
           this.appendCharToString(this.ch);
         }
-        i++;
       }
-  
-      // Push any last remaining token
-      if (this.token.length > 0) {
-        tokens.push(this.token);
-        
-        // Count keyword occurrences for the last token
-        if (this.token.toLowerCase() === keyword.toLowerCase()) {
-          keywordCount++;
-        }
-      }
-  
-      return { tokens, keywordCount };
+      i++;
     }
+
+    // Push any last remaining token
+    if (this.token.length > 0) {
+      tokens.push(this.token);
+
+      // Count keyword occurrences for the last token
+      if (this.token.toLowerCase() === keyword.toLowerCase()) {
+        keywordCount++;
+      }
+    }
+
+    return { tokens, keywordCount };
   }
+}
+
 module.exports = Tokenizer;  
